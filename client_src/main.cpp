ae2158b10c5d152ec2d9967521ae1b7f0b65c918
Trytu035 <lokalizacja11@wp.pl>
2026-03-28 00:10:23 +0100

    feature key database server and client

--- client_src/main.cpp
+++ client_src/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <boost/asio.hpp>
+#include <nlohmann/json.hpp>
 #include <array>
+#include "../communication.h"
 
 int main(int argc, char **argv){
     try{
         boost::asio::io_context io_ctx;
-        // boost::asio::io::address server_address = boost::asio::ip::make_address("localhost")
         boost::asio::ip::tcp::socket connection(io_ctx);
 
         connection.connect(
@@ -17,18 +18,42 @@ int main(int argc, char **argv){
             )
         );
 
-        std::array<char, 128> buf;
+        query q;
+        if (argc == 1){
+            q = {query::action_type::GET, 123456789};
+        } else if (argc == 2){
+            q = {query::action_type::GET, atoi(argv[1])};
+        } else if (argc == 3){
+            nlohmann::json this_action_type = argv[1];
+            q = {this_action_type.get<query::action_type>(), atoi(argv[2])};
+        }else {
+            throw std::exception("skladnia: Lab1_client.exe [[get | set] key]\n"
+                 "np.: Lab1_client.exe 123456789\n"
+                 "Lab1_client.exe set 20\n"
+                "klucz 0 jest zarezerwowany\n");
+        }
 
-        boost::system::error_code error;
+        nlohmann::json data = q;
+        std::array<char, 128> buf{};
+
+        boost::system::error_code error{};
+        
+        boost::asio::write(connection, boost::asio::buffer(data.dump()));
         size_t len = connection.read_some(boost::asio::buffer(buf), error);
+        nlohmann::json result = nlohmann::json::parse(std::string(buf.data(), len));
+        auto res = result.get<answer>();
+
         if (error == boost::asio::error::eof){
             std::cout << "eof" << std::endl;
+            // break;
         }
-            // break; // Connection closed cleanly by peer.
-        else if (error)
+        else if (error){
             throw boost::system::system_error(error); // Some other error.
-
+        }
         std::cout.write(buf.data(), len);
+        std::cout << std::endl;
+
+        std::cout << "closing\n" << std::endl;
         connection.close();
     } catch (std::exception &e){
         std::cout << e.what() << std::endl;
--- server_src/main.cpp
+++ server_src/main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
-// #include <net   ioapi.h>
-// #include <sys/types.h>
-// #include <winsock.h>
-// #include <winsock2.h>
-// #include <net
 #include <boost/asio.hpp>
-// #include <boost/beast.hpp>
+#include "../communication.h"
+#include <map>
+
+#define STORAGE_PASSWORD "tajneHaslo123"
+
+std::map<int, std::string> storage;
 
 int main(int argc, char **argv){
     // std::socket s;
+    storage.insert({123456789, "tajneHaslo123"});
+
     try{
         boost::asio::io_context io_ctx; //async execution context
         boost::asio::ip::tcp::acceptor server_socket(
@@ -19,14 +21,54 @@ int main(int argc, char **argv){
             )
         );
         // boost::asio::ip::tcp::socket socket;
+        std::string message = "";
+        std::array<char, 128> buf;
         while (true){
             boost::asio::ip::tcp::socket connection(io_ctx);
             server_socket.accept(connection);
-
-            std::string message = "ack";
             boost::system::error_code ignored_error;    // temporarily
-            boost::asio::write(connection, boost::asio::buffer(message), ignored_error);
-            // boost::asio::ip::tcp::socket client(boost::asio::execution::any_executor());
+            boost::system::error_code timeout_error;
+
+            size_t len = connection.read_some(boost::asio::buffer(buf), timeout_error);
+            nlohmann::json result = nlohmann::json::parse(std::string(buf.data(), len));
+            query q = result.get<query>();
+            std::cout << q.action << " " << q.key << std::endl;
+            std::cout.write(buf.data(), len) << std::endl;
+            
+            nlohmann::json send{};
+
+            switch (q.action){
+                case query::action_type::GET:
+                    if (q.key != 0){
+                        auto found = storage.find(q.key);
+                        if (found != storage.end()){
+                            answer packet(answer::status_type::OK, q.key, true);
+                            send = packet;
+                        }else{
+                            answer packet(answer::status_type::OK, q.key, false);
+                            send = packet;
+                        }
+                        boost::asio::write(connection, boost::asio::buffer(send.dump()), ignored_error);
+                    } else{
+                        //TODO: send error packet
+                    }
+                    break;
+                case query::action_type::SET:
+                    if (q.key != 0){
+                        if (q.password == STORAGE_PASSWORD){
+                            answer packet(answer::status_type::CREATED, q.key, false, "Key added successfully");
+                            send = packet;
+                        }else{
+                            answer packet(answer::status_type::ERR, 0, false, "Authentication failed");
+                            send = packet;
+                        }
+                        boost::asio::write(connection, boost::asio::buffer(send.dump()), ignored_error);
+                    } else{
+                        //TODO: send error packet
+                    }
+            }
+            
+            std::cout << "end connection\n";
         }
         // s.connect()
         // boost::asio::ip::tcp;  
